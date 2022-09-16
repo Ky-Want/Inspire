2e29b65e0e17d053e8773edbfcfccda9a6e1ee82
Lumine3449 <>
2022-09-16 13:51:02 -0600

    html final push

--- app/AppState.js
+++ app/AppState.js
@@ -1,12 +1,22 @@
-import { Value } from "./Models/Value.js"
 import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
 import { loadState } from "./Utils/Store.js"
 
+
+
+
 class AppState extends EventEmitter {
   /** @type {import('./Models/Value').Value[]} */
-  values = loadState('values', Value)
+  // values = loadState('values', Value)
+
+
 }
+
+
+
+
+
+
 
 export const appState = new Proxy(new AppState(), {
   get(target, prop) {
